dd861cd22a934ab2c4cb029b4842c27bc3ea3495
Davi Monteiro <montsgit@gmail.com>
2025-03-10 17:44:24 -0300

    Changes

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisteredUserController;
-
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/user', [UserController::class, 'index']);
 
 Route::get('/register', [RegisteredUserController::class, 'showForm']);
 Route::post('/register', [RegisteredUserController::class, 'register']);
