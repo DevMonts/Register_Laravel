6fc1c9c1d1653fef2581ee28cd18c0e7791ad545
Davi Monteiro <montsgit@gmail.com>
2025-03-12 16:45:33 -0300

    Route Correction

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,12 @@ use App\Http\Controllers\RegistrarUsuarioController;
 Route::get('/', function () {
     return view('login');
 })->name('login');
+
 Route::get('/registro', [RegistrarUsuarioController::class, 'showForm']);
 Route::post('/registro', [RegistrarUsuarioController::class, 'registro']);
 
-Route::view('/painel', 'dashboard')->middleware('auth')->name('painel');
 Route::post('/client_painel', [LogarUsuarioController::class, 'login']);
+
+Route::get('/client_list', function () {
+    return view('client_list');
+})->name('client_list');
