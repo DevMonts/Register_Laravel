eb134b28e6f35bdd28f34dd4ddbffbbe256f926c
Davi Monteiro <montsgit@gmail.com>
2025-03-13 11:04:43 -0300

    Request Routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ClienteController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegistrarUsuarioController;
 use App\Http\Controllers\LogarUsuarioController;
@@ -13,4 +14,13 @@ Route::get('/registro', [RegistrarUsuarioController::class, 'showForm']);
 Route::post('/registro', [RegistrarUsuarioController::class, 'registro']);
 
 Route::post('/home', [LogarUsuarioController::class, 'login']);
+
 Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::get('/clients', [ClienteController::class, 'index'])->name('clients.index');
+Route::get('/clients/create', [ClienteController::class, 'create'])->name('clients.create');
+Route::get('/clients', [ClienteController::class, 'store'])->name('clients.store');
+Route::get('/clients/{client}', [ClienteController::class, 'show'])->name('clients.show');
+Route::get('/clients/{client}/edit', [ClienteController::class, 'edit'])->name('clients.edit');
+Route::get('/clients/{client}', [ClienteController::class, 'update'])->name('clients.update');
+Route::get('/clients/{client}', [ClienteController::class, 'destroy'])->name('clients.destroy');
