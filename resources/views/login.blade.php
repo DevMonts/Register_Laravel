a22bfb913420e663ccdaedfb60e65bd230bf694c
Davi Monteiro <montsgit@gmail.com>
2025-03-18 11:25:17 -0300

    Login error logic correction

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -50,13 +50,6 @@
                         {{ $errors->first('password') }}
                     </div>
                 @endif
-                <div class="text-red-500">
-                    @if ($errors->any())
-                        @foreach ($errors->all() as $error)
-                            {{ $error }}
-                        @endforeach
-                    @endif
-                </div>
                 <div>
                     <button type="submit"
                         class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
