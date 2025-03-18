808e7ca6bc31e9abd6701d63166db84961b9d51c
Davi Monteiro <montsgit@gmail.com>
2025-03-18 10:55:10 -0300

    Delete no use import

--- app/Http/Controllers/LogarUsuarioController.php
+++ app/Http/Controllers/LogarUsuarioController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Validator;
 
 class LogarUsuarioController extends Controller
 {
