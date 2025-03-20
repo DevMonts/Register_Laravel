578084d1b0b87f8f02ab745ec8c1c153bfa2481d
Davi Monteiro <montsgit@gmail.com>
2025-03-20 16:11:08 -0300

    Call the products on the controller

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -14,6 +14,7 @@ class ProdutoController
     public function index()
     {
         $user = Auth::user();
+        $product = Product::all();
         return view('home', compact('user'));
     }
 
